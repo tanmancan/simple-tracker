51691e4b081b485b8527df3d53d15c69fee2c03e
tanmancan <contact@tkarimdesign.com>
2018-04-13 16:06:39 -0400

    Add action for stopping timer

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 export const ADD_TIMER = 'ADD_TIMER';
 export const DELETE_TIMER = 'DELETE_TIMER';
 export const UPDATE_TIMER = 'UPDATE_TIMER';
+export const STOP_TIMER = 'STOP_TIMER';
 
 export function addTimer({timerState, id}) {
   return {type: ADD_TIMER, timerState, id}
@@ -12,4 +13,8 @@ export function deleteTimer({timerState, id}) {
 
 export function updateTimer({timerState, id}) {
   return {type: UPDATE_TIMER, timerState, id}
+}
+
+export function stopTimer({timerState, id}) {
+  return {type: STOP_TIMER, timerState, id}
 }
