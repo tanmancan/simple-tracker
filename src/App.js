0706e55f27428de8cdc1bf1ecc54bf369f109c7e
tanmancan <contact@tkarimdesign.com>
2018-04-13 16:11:06 -0400

    Update main layout, move entrypoints for container helpers into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React, { Component } from 'react';
-import TimeCardList from "./containers/TimeCardList";
+import TimeCardListBuilder from './components/TimeCardListBuilder';
+import Controls from './components/Controls';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.sortByWeight = this.sortByWeight.bind(this);
+  }
+
+  controls() {
+    return React.createElement(
+      Controls, {
+        ...this.props
+      }
+    );
+  }
+
+  listBuilder() {
+    return React.createElement(
+      TimeCardListBuilder, {
+        ...this.props
+      }
+    )
+  }
+
+  sortByWeight() {
+    return this;
+  }
+
+  appStyle() {
+    return {
+      height: '100vh'
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <TimeCardList></TimeCardList>
+      <div className="App" style={this.appStyle()}>
+        {this.controls()}
+        <main>
+          <div className="nav-wrapper">
+            <nav className="top-nav z-depth-0">
+              <div className="nav-wrapper red">
+                <div className="col s12">
+                  <a href="#open-menu" data-target="slide-out" className="sidenav-trigger white-text"><i className="material-icons">menu</i></a>
+                  <a href="/" className="brand-logo center white-text lighten-4-text">React-Timer</a>
+                </div>
+              </div>
+            </nav>
+          </div>
+          <div className="row">
+            <div className="col s12">
+              {this.listBuilder()}
+            </div>
+          </div>
+        </main>
       </div>
     );
   }
