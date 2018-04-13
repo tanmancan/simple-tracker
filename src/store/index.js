2cb976c4c8babd591ce5461923f0b17723b2d270
tanmancan <contact@tkarimdesign.com>
2018-04-13 16:07:42 -0400

    Add local storage and devtool support

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,16 @@
 import { createStore } from "redux";
 import { timerApp } from "./reducers";
 
-const store = createStore(timerApp);
+const savedTimerState = JSON.parse(window.localStorage.getItem('timerApp'));
+const store = createStore(
+  timerApp,
+  savedTimerState || {},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+  let savedState = JSON.stringify(store.getState());
+  window.localStorage.setItem('timerApp', savedState);
+});
 
 export default store;
