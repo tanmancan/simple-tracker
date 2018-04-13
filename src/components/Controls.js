93194a45ce1d1fe85346a3b6f16dd34fabf00b4d
tanmancan <contact@tkarimdesign.com>
2018-04-13 16:09:32 -0400

    Update layout to sidenav

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {formatTime} from './Timer';
 
 export default class Controls extends Component {
   constructor(props) {
     super(props);
     this.addTimer = this.addTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    console.log(formatTime(this.props.getTotalTime));
   }
 
   addTimer() {
@@ -14,11 +17,45 @@ export default class Controls extends Component {
     });
   }
 
+  stopTimer() {
+    console.log(this.props.getAllTimerStates[this.props.getActiveTimer[0]]);
+    this.props.onTimerStop({
+      timerState: this.props.getAllTimerStates[this.props.getActiveTimer[0]],
+      id: this.props.getActiveTimer[0]
+    });
+  }
+
   render() {
     return (
-      <div className="timer-controls">
-        <button onClick={this.addTimer}>Add Timer</button>
-      </div>
+      <ul id="slide-out" className="sidenav sidenav-fixed z-depth-0">
+        <li>
+          <div className="user-view">
+            <div className="background red">
+            </div>
+            <a href="/">
+              <i className="material-icons circle white red-text sidenav-logo">alarm</i>
+            </a>
+            <a href="/"><span className="white-text name">React-Timer</span></a>
+            <a href="https://www.tkarimdesign.com"><span className="white-text email">www.tkarimdesign.com</span></a>
+            <a href="#total-time">
+              <span className="white-text">Total – {formatTime(this.props.getTotalTime)}</span>
+            </a>
+          </div>
+        </li>
+        <li>
+          <a onClick={this.addTimer} className="brown-text lighten-4-text">
+            <i className="material-icons">add_alarm</i>
+            Add Timer
+          </a>
+        </li>
+        <li>
+          <a onClick={this.stopTimer} className="brown-text lighten-4-text">
+            <i className="material-icons">alarm_off</i>
+            Stop Timer
+          </a>
+        </li>
+        <li><div className="divider"></div></li>
+      </ul>
     );
   }
 }
