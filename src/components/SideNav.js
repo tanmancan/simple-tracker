2b3520bdd4d21bbac950d0c8d935452c1f0b9ee3
tanmancan <contact@tkarimdesign.com>
2018-04-15 21:30:22 -0400

    Rename controls to sidenav

---
+++ src/components/SideNav.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import SideNavHeader from './SideNavHeader';
+
+export default class SideNav extends Component {
+  constructor(props) {
+    super(props);
+    this.addTimer = this.addTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
+
+  addTimer() {
+    let uid = +`${Math.floor(Math.random() * 1000)}${+new Date()}`;
+    this.props.onTimerAdd({
+      timerState: {},
+      id: `timer-${uid}`
+    });
+  }
+
+  stopTimer() {
+    this.props.onTimerStop({
+      timerState: this.props.getAllTimerStates[this.props.getActiveTimer[0]],
+      id: this.props.getActiveTimer[0]
+    });
+  }
+
+  render() {
+    return (
+      <ul id="slide-out" className="sidenav sidenav-fixed z-depth-0">
+        <SideNavHeader getTotalTime={this.props.getTotalTime}></SideNavHeader>
+        <li>
+          <a href="#add-timer" onClick={this.addTimer} className="brown-text lighten-4-text">
+            <i className="material-icons">add_alarm</i>
+            Add Timer
+          </a>
+        </li>
+        <li>
+          <a href="#stop-timer" onClick={this.stopTimer} className="brown-text lighten-4-text">
+            <i className="material-icons">alarm_off</i>
+            Stop Timer
+          </a>
+        </li>
+        <li><div className="divider"></div></li>
+      </ul>
+    );
+  }
+}
