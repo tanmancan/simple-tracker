1dfeb94ef16a5a2dff767533776c5d7dd59dd242
tanmancan <contact@tkarimdesign.com>
2018-04-19 10:57:23 -0400

    implement separate states for tag and timer

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,26 @@
-import { createStore } from "redux";
-import { timerApp } from "./reducers/timer";
+import { createStore, combineReducers } from "redux";
+import { timerState } from "./reducers/timer";
+import { tagState } from "./reducers/tags";
 
-const savedTimerState = JSON.parse(window.localStorage.getItem('timerApp'));
+const savedTimerState = JSON.parse(window.localStorage.getItem('timerState'));
+const savedTagState = JSON.parse(window.localStorage.getItem('tagState'));
+const rootReducer = combineReducers({timerState, tagState});
+const savedState = {
+  timerState: savedTimerState || {},
+  tagState: savedTagState || {}
+};
 const store = createStore(
-  timerApp,
-  savedTimerState || {},
+  rootReducer,
+  savedState || {},
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 store.subscribe(() => {
-  let savedState = JSON.stringify(store.getState());
-  window.localStorage.setItem('timerApp', savedState);
+  let state = store.getState();
+  let savedTimerState = JSON.stringify(state.timerState);
+  window.localStorage.setItem('timerState', savedTimerState);
+  let savedTagState = JSON.stringify(state.tagState);
+  window.localStorage.setItem('tagState', savedTagState);
 });
 
 export default store;
