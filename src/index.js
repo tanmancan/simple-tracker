7172b304155149f00c148d65b1a78c6b49f1a1e0
tanmancan <contact@tkarimdesign.com>
2018-04-22 23:10:08 -0400

    Remove old fonts, add new one, update theme color

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import store from './store';
 import './index.css';
+import './assets/iconfont/material-icons.css';
+import './assets/lato/lato.css';
 import TimerApp from './containers/TimerApp';
 import registerServiceWorker from './registerServiceWorker';
 import 'materialize-css/dist/css/materialize.min.css';
