22de1bd7d93128d532945bb7c830a96a58353503
tanmancan <contact@tkarimdesign.com>
2018-04-26 08:13:23 -0400

    update test

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import store from './store';
-import * as timerActions from './store/actions/timer';
-import * as tagActions from './store/actions/tags';
-import './index.css';
-import TimerApp from './containers/TimerApp';
-import registerServiceWorker from './registerServiceWorker';
+import store from '../store';
+import * as timerActions from '../store/actions/timer';
+import * as tagActions from '../store/actions/tags';
+import '../index.css';
+import TimerApp from '../containers/TimerApp';
+import registerServiceWorker from '../registerServiceWorker';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 
